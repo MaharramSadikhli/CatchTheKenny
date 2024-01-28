bc13131c3a67b0a59bf39cef37c3365a885aaf36
Muarem <s.muarem10@gmail.com>
2024-01-29 01:00:03 +0400

    score increasing fun created

--- app/src/main/java/com/imsoft/catchthekenny/MainActivity.kt
+++ app/src/main/java/com/imsoft/catchthekenny/MainActivity.kt
@@ -2,10 +2,22 @@ package com.imsoft.catchthekenny
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.imsoft.catchthekenny.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private var score: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun setScore(view: View) {
+        score += 1
+        binding.scoreView.text = score.toString()
     }
 }
